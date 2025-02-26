01fcdf7030f89116df000089d7aa84b0de93085e
felipe12l <andres.luna01@uptc.edu.co>
2025-02-26 15:26:23 -0500

    metodo de biseccion funcional

--- backend/biseccion/bisection.py
+++ backend/biseccion/bisection.py
@@ -1,6 +1,18 @@
-import numpy as np
-class bisection:
-    def __init__(function):
-        self.function=function
 
-        
+
+def bisection(function:str,point1,point2, iteration):
+    f=eval(f"lambda x:{function}")
+    iteration-=1
+    middle=(point1+point2)/2
+    if iteration==0:
+          return [middle,0]
+    if f(middle)==0:
+        return [middle,iteration]
+    if f(point1)*f(middle)<0:
+        return bisection(function,point1,middle,iteration)
+    else:
+        return bisection(function,middle,point2,iteration)
+
+
+     
+            
