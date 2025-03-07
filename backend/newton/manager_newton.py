086d5dcbdfda7016c2578eebc5aad7f4792d1dcb
felipe12l <andres.luna01@uptc.edu.co>
2025-03-06 21:01:27 -0500

    metodo de newton creado

--- backend/newton/manager_newton.py
+++ backend/newton/manager_newton.py
@@ -1,16 +1,28 @@
-from sympy import symbols, diff, Derivative
+from sympy import symbols, diff, lambdify,sympify
 
 class newton:
-    def __init__(self,function:str,point,iterations):
-        self.function=function
+    def __init__(self,function:str,point,iterations,var):
+        self.var=symbols(f"{var}")
+        self.expr = sympify(function)
+        self.function=lambdify(self.var,function)
         self.point=point
         self.iterations=iterations
-    def derivate(self):
-        try:
-            x=symbols("x")
-            return derivate_func=Derivative(self.function,{"x":x}).doit
+        
+        
+    def __derivate(self):
+        der_expr = diff(self.expr, self.var)  
+        return lambdify(self.var, der_expr)  
 
-        except ValueError:
-            print(ValueError)
+            
     def calculate_newton(self):
-        der=self.derivate()
+        der=self.__derivate()
+        
+        while True:
+            if self.iterations==0:
+                break
+            self.point=self.point-self.function(self.point)/der(self.point)
+            self.iterations-=1
+        return self.point
+newtons=newton("x**3-x-1",1,10,"x")
+print(newtons.calculate_newton())
+        
