4e620e9977d8f15899b3039ea1809fbffaca9ef9
felipe12l <andres.luna01@uptc.edu.co>
2025-04-23 22:15:11 -0500

    modificacion front

--- frontend/components/app.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "https://esm.sh/react-dom@18.2.0/client"
-
-const appDomElement=document.getElementById("root")
-
-const root= ReactDOM.createRoot(appDomElement)
-
-root.render('calculadora')
